9514ba795de9c52f9989b00ca87b1be2014ef855
vpadilhafps <vpadilhabr1@gmail.com>
2024-05-08 09:36:42 -0300

    Correção exercicio 16

--- aula15/script.js
+++ aula15/script.js
@@ -1,19 +1,27 @@
-function contar(){
+function contar() {
     var inicio = document.getElementById('txti')
     var fim = document.getElementById('txtf')
-    var passo = document.getElementById('txtp')   
+    var passo = document.getElementById('txtp')
     var res = document.querySelector('div#res')
 
-        if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
-            window.alert('[ERRO] Faltam dados!')
-        } else {
-            res.innerHTML = 'Contando: '
-            var i = Number(inicio.value)
-            var f = Number(fim.value)
-            var p = Number(passo.value)
-
-            for(i = i; i <= f; i += p){
+    if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        window.alert('[ERRO] Faltam dados!')
+    } else {
+        res.innerHTML = 'Contando: '
+        var i = Number(inicio.value)
+        var f = Number(fim.value)
+        var p = Number(passo.value)
+        if (i < f) {
+        //Contagem crescente
+            for (i = i; i <= f; i += p) {
                 res.innerHTML += `${i} \u{1F449} `
             }
+        } else {
+        //Contagem decrescente
+            for (var i = i; i >= f; i -=p){
+                res.innerHTML += `${i} \u{1F449} `
+            }
+            res.innerHTML += `${i} \u{1F3C1} `
         }
+    }
 }
